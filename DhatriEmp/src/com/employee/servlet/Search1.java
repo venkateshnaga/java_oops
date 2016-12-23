1088e306aca088057b5a00ee5035e24f49ac8ac5
venkateshnaga <venkatesh.dhatriinfo@gmail.com>
2016-12-23 15:51:26 +0530

    none

--- DhatriEmp/src/com/employee/servlet/Search1.java
+++ DhatriEmp/src/com/employee/servlet/Search1.java
@@ -45,7 +45,27 @@ public class Search1 extends HttpServlet {
 		
 		
 		String searchElement=request.getParameter("dropdown");
-		
+		<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+			    pageEncoding="ISO-8859-1"%>
+			<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+			<html>
+			<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+			<title>Insert title here</title>
+			</head>
+			<body align="center">
+			<form action="Search1" method="post">
+			View By EmployeeId|Name|Salary:
+			<select name="dropdown">
+			 <option value="id">Id</option>
+			<option value="name">Name</option>
+			<option value="sal">Salary</option>
+			</select>
+			<input type="text" name="emp"/>
+			<input type="submit" value="submit"/>
+			</form>
+			</body>
+			</html>
 		String temp=request.getParameter("emp");
 		
 		if(searchElement.equals("id"))
